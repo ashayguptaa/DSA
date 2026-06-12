27a1e4247cc9d8bbadb7ee3c5cadff46a71e0bbf
Ashay <guptaashay18@gmail.com>
2026-06-12 20:05:50 +0530

    Time: 151 ms (34.45%) | Memory: 17.4 MB (58.52%) - LeetSync

--- Binary_Trees/416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ Binary_Trees/416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
+    
+    bool subsetsum(vector<int>& nums, int target){
+        int n = nums.size();
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
+        for(int i=0;i<n;i++)
+            dp[i][0] = true;
+        if(nums[0] <= target)
+            dp[0][nums[0]] = true;
+
+        for(int i=1;i<n;i++){
+            for(int t=1;t<=target;t++){
+                bool notTake = dp[i-1][t];
+                bool Take = false;
+                if(nums[i] <= t){
+                    Take = dp[i-1][t-nums[i]];
+                }
+                dp[i][t] = Take || notTake;
+            }
+        }
+        return dp[n-1][target];
+    }
+
     bool canPartition(vector<int>& nums) {
         int totalSum = accumulate(begin(nums), end(nums), 0);
-        if (totalSum & 1) return false;
-
-        int halfSum = totalSum / 2;
-        bitset<10001> dp(1); // dp[0] = true
-
-        for (int num : nums)
-            dp |= dp << num;
-
-        return dp[halfSum];
+        if(totalSum % 2 == 1) return false;
+        int target = totalSum/2;
+        return subsetsum(nums,target);
     }
 };
